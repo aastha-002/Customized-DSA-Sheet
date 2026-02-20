12449e52e0366908a8906facb9a1fae9d0da8114
Aastha <aasthajasuja2020@gmail.com>
2026-02-20 10:15:53 +0530

    Remove Outermost Parentheses (Leetcode-1021)

---
+++ String/05.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        int sum=0;
+        int start=0;
+        int end=0;
+
+        StringBuilder ans=new StringBuilder();
+        while(end<s.length()){
+            if(s.charAt(end)=='('){
+                sum++;
+            }
+            else{
+                sum--;
+            }
+            if(sum==0){
+                ans.append(s.substring(start+1,end));
+                start=end+1;
+            }
+            end++;
+        }
+        return ans.toString();
+        
+    }
+}
